7d545e7b10a62591158148d931885f169b72d6a9
Danil <danig1991@mail.ru>
2024-11-22 20:04:58 +0300

    ДЗ: Создание модуля Авторизации

--- login_page.py
+++ login_page.py
@@ -9,25 +9,22 @@ class LoginPage:
 
     def authorization(self, user_name, password):
         # ввод Username
-        input_user_name = WebDriverWait(self.driver_chrome, 30).until(EC.element_to_be_clickable((
-            By.ID,
-            "user-name"
-        )))
+        input_user_name = WebDriverWait(self.driver_chrome, 30).until(
+            EC.element_to_be_clickable((By.ID, "user-name"))
+        )
         input_user_name.send_keys(user_name)
         print("Ввод Username.")
 
         # ввод Password
-        input_password = WebDriverWait(self.driver_chrome, 30).until(EC.element_to_be_clickable((
-            By.ID,
-            "password"
-        )))
+        input_password = WebDriverWait(self.driver_chrome, 30).until(
+            EC.element_to_be_clickable((By.ID, "password"))
+        )
         input_password.send_keys(password)
         print("Ввод Password.")
 
         # нажатие на кнопку Login
-        click_login = WebDriverWait(self.driver_chrome, 30).until(EC.element_to_be_clickable((
-            By.ID,
-            "login-button"
-        )))
+        click_login = WebDriverWait(self.driver_chrome, 30).until(
+            EC.element_to_be_clickable((By.ID, "login-button"))
+        )
         click_login.click()
         print("Нажатие на кнопку Login.")
